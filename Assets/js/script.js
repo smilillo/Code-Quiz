0cc168d5064417dfd8c7c67e9fdb212930f40bf7
smilillo <sofiemilillo@gmail.com>
2023-02-16 19:27:56 -0500

    added in questions to js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,15 @@
 function startQuiz() {
   let startButton = "";
+  // hide start screen
+  let startScreenEl = document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'hide');
+  // un-hide questions section
+  questionsEl.removeAttribute('class', 'hide');
 }
 
 startButton.addEventListener.on("click", startQuiz());
 
-function runQuestions()
+function runQuestions() {
 
 let questions = [
   {
@@ -16,15 +21,29 @@ let questions = [
     title: 'The condition in an if / else statement is enclosed within ____.',
     choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
     answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+    answer: 'all of the above',
+  },
+  {
+    title: 'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title: 'A very useful tool used during development and debugging for printing content to the debugger is ____.',
+    choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+    answer: 'console.log',
   }
 ]
+}
 
-// hide start screen
-  let startScreenEl = document.getElementById('start-screen');
-  startScreenEl.setAttribute('class', 'hide');
+function finalScore () {
+    let timerCount = timerCount.value;
 
-// un-hide questions section
-  questionsEl.removeAttribute('class');
+}
 
 // object for high scores to store to local storage
 //   let highScore = {
