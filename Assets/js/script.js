4ee58cb919b56ef86d87a19c0ad64d96da86eb3f
smilillo <sofiemilillo@gmail.com>
2023-02-21 22:47:47 -0500

    fixed multiplying buttons! also adjsted answer values so that correct and incorrect response is accurate

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -24,27 +24,27 @@ let questionsArr = [
     {
       title: 'Commonly used data types DO NOT include:',
       choices: ['strings', 'booleans', 'alerts', 'numbers'],
-      answer: '2',
+      answer: '3',
     },
     {
       title: 'The condition in an if / else statement is enclosed within ____.',
       choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-      answer: '2',
+      answer: '3',
     },
     {
       title: 'Arrays in JavaScript can be used to store ____.',
       choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-      answer: '3',
+      answer: '4',
     },
     {
       title: 'String values must be enclosed within ____ when being assigned to variables.',
       choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-      answer: '2',
+      answer: '3',
     },
     {
       title: 'A very useful tool used during development and debugging for printing content to the debugger is ____.',
       choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
-      answer: '3',
+      answer: '4',
     }
 ];
 
@@ -77,9 +77,8 @@ function startQuiz() {
 
 // Present Questions after Click Event
 function runQuestions() {
-    // for ( let i = 0; i < questionsArr[questionIndex].title.length; i++) {
         questionTextEl.textContent = questionsArr[questionIndex].title;
-    // }
+        answersEl.textContent = "";
     for (let i = 0; i < questionsArr[questionIndex].choices.length; i++) {
         let choiceBtn = document.createElement("button");
         choiceBtn.textContent = questionsArr[questionIndex].choices[i];
